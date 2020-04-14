15802489959ce34dfc68dd77527497717ea8af92
dkhale <62896484+dkhale@users.noreply.github.com>
2020-04-14 11:58:13 -0400

    Add files via upload

---
+++ Code/DriverModel_DLL_HV/DriverModel.cpp
@@ -0,0 +1,298 @@
+﻿/*==========================================================================*/
+/*  DriverModel.cpp                                  DLL Module for VISSIM  */
+/*                                                                          */
+/*  Interface module for external driver models.                            */
+/*  Dummy version that does nothing (uses Vissim's internal model).         */
+/*  export HV trajectory information                                        */
+/*==========================================================================*/
+
+#include "DriverModel.h"
+#include <stdio.h>
+#include <iostream> 
+#include <fstream>
+#include <list>
+#include <math.h>
+#include <ctime>
+#include <map>  
+#include <string> 
+#include <stdlib.h>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+/*==========================================================================*/
+
+
+using namespace std;
+using std::vector;
+vector<long>::iterator iter;
+
+vector<vector<float>> veh_data(500);
+int www = 0;
+
+
+double  desired_acceleration = 0.0;
+double  desired_lane_angle   = 0.0;
+long    active_lane_change   = 0;
+long    rel_target_lane      = 0;
+double  desired_velocity     = 0.0;
+long    turning_indicator    = 0;
+long    vehicle_color        = RGB(0,0,0);
+long veh_type;
+double  current_speed = 0.0;
+long veh_id;
+double time_run;
+
+ofstream fout;
+ifstream fin;
+
+
+
+/*==========================================================================*/
+
+BOOL APIENTRY DllMain (HANDLE  hModule,
+                       DWORD   ul_reason_for_call,
+                       LPVOID  lpReserved)
+{
+  switch (ul_reason_for_call) {
+      case DLL_PROCESS_ATTACH:
+      case DLL_THREAD_ATTACH:
+      case DLL_THREAD_DETACH:
+      case DLL_PROCESS_DETACH:
+         break;
+  }
+  return TRUE;
+}
+
+/*==========================================================================*/
+
+DRIVERMODEL_API  int  DriverModelSetValue (long   type,
+                                           long   index1,
+                                           long   index2,
+                                           long   long_value,
+                                           double double_value,
+                                           char   *string_value)
+{
+  /* Sets the value of a data object of type <type>, selected by <index1> */
+  /* and possibly <index2>, to <long_value>, <double_value> or            */
+  /* <*string_value> (object and value selection depending on <type>).    */
+  /* Return value is 1 on success, otherwise 0.                           */
+
+  switch (type) {
+    case DRIVER_DATA_PATH                   :
+    case DRIVER_DATA_TIMESTEP               :
+    case DRIVER_DATA_TIME                   :
+		time_run = double_value;
+      return 1;
+    case DRIVER_DATA_USE_UDA                :
+      return 0; /* doesn't use any UDAs */
+                /* must return 1 for desired values of index1 if UDA values are to be sent from/to Vissim */
+    case DRIVER_DATA_VEH_ID                 :
+		veh_id = long_value;
+		return 1;
+    case DRIVER_DATA_VEH_LANE               :
+    case DRIVER_DATA_VEH_ODOMETER           :
+    case DRIVER_DATA_VEH_LANE_ANGLE         :
+    case DRIVER_DATA_VEH_LATERAL_POSITION   :
+    case DRIVER_DATA_VEH_VELOCITY           :
+		current_speed = double_value;
+		return 1;
+    case DRIVER_DATA_VEH_ACCELERATION       :
+    case DRIVER_DATA_VEH_LENGTH             :
+    case DRIVER_DATA_VEH_WIDTH              :
+    case DRIVER_DATA_VEH_WEIGHT             :
+    case DRIVER_DATA_VEH_MAX_ACCELERATION   :
+      return 1;
+    case DRIVER_DATA_VEH_TURNING_INDICATOR  :
+      turning_indicator = long_value;
+      return 1;
+    case DRIVER_DATA_VEH_CATEGORY           :
+    case DRIVER_DATA_VEH_PREFERRED_REL_LANE :
+    case DRIVER_DATA_VEH_USE_PREFERRED_LANE :
+      return 1;
+    case DRIVER_DATA_VEH_DESIRED_VELOCITY   :
+      desired_velocity = double_value;
+      return 1;
+    case DRIVER_DATA_VEH_X_COORDINATE       :
+    case DRIVER_DATA_VEH_Y_COORDINATE       :
+    case DRIVER_DATA_VEH_Z_COORDINATE       :
+    case DRIVER_DATA_VEH_REAR_X_COORDINATE  :
+    case DRIVER_DATA_VEH_REAR_Y_COORDINATE  :
+    case DRIVER_DATA_VEH_REAR_Z_COORDINATE  :
+    case DRIVER_DATA_VEH_TYPE               :
+		veh_type = long_value;
+		return 1;
+    case DRIVER_DATA_VEH_COLOR              :
+      vehicle_color = long_value;
+      return 1;
+    case DRIVER_DATA_VEH_CURRENT_LINK       :
+      return 0; /* (To avoid getting sent lots of DRIVER_DATA_VEH_NEXT_LINKS messages) */
+                /* Must return 1 if these messages are to be sent from VISSIM!         */
+    case DRIVER_DATA_VEH_NEXT_LINKS         :
+    case DRIVER_DATA_VEH_ACTIVE_LANE_CHANGE :
+    case DRIVER_DATA_VEH_REL_TARGET_LANE    :
+    case DRIVER_DATA_VEH_INTAC_STATE        :
+    case DRIVER_DATA_VEH_INTAC_TARGET_TYPE  :
+    case DRIVER_DATA_VEH_INTAC_TARGET_ID    :
+    case DRIVER_DATA_VEH_INTAC_HEADWAY      :
+    case DRIVER_DATA_VEH_UDA                :
+    case DRIVER_DATA_NVEH_ID                :
+    case DRIVER_DATA_NVEH_LANE_ANGLE        :
+    case DRIVER_DATA_NVEH_LATERAL_POSITION  :
+    case DRIVER_DATA_NVEH_DISTANCE          :
+    case DRIVER_DATA_NVEH_REL_VELOCITY      :
+    case DRIVER_DATA_NVEH_ACCELERATION      :
+    case DRIVER_DATA_NVEH_LENGTH            :
+    case DRIVER_DATA_NVEH_WIDTH             :
+    case DRIVER_DATA_NVEH_WEIGHT            :
+    case DRIVER_DATA_NVEH_TURNING_INDICATOR :
+    case DRIVER_DATA_NVEH_CATEGORY          :
+    case DRIVER_DATA_NVEH_LANE_CHANGE       :
+    case DRIVER_DATA_NVEH_TYPE              :
+    case DRIVER_DATA_NVEH_UDA               :
+    case DRIVER_DATA_NO_OF_LANES            :
+    case DRIVER_DATA_LANE_WIDTH             :
+    case DRIVER_DATA_LANE_END_DISTANCE      :
+    case DRIVER_DATA_RADIUS                 :
+    case DRIVER_DATA_MIN_RADIUS             :
+    case DRIVER_DATA_DIST_TO_MIN_RADIUS     :
+    case DRIVER_DATA_SLOPE                  :
+    case DRIVER_DATA_SLOPE_AHEAD            :
+    case DRIVER_DATA_SIGNAL_DISTANCE        :
+    case DRIVER_DATA_SIGNAL_STATE           :
+    case DRIVER_DATA_SIGNAL_STATE_START     :
+    case DRIVER_DATA_SPEED_LIMIT_DISTANCE   :
+    case DRIVER_DATA_SPEED_LIMIT_VALUE      :
+      return 1;
+    case DRIVER_DATA_DESIRED_ACCELERATION :
+      desired_acceleration = double_value;
+      return 1;
+    case DRIVER_DATA_DESIRED_LANE_ANGLE :
+      desired_lane_angle = double_value;
+      return 1;
+    case DRIVER_DATA_ACTIVE_LANE_CHANGE :
+      active_lane_change = long_value;
+      return 1;
+    case DRIVER_DATA_REL_TARGET_LANE :
+      rel_target_lane = long_value;
+      return 1;
+    default :
+      return 0;
+  }
+}
+
+/*--------------------------------------------------------------------------*/
+
+DRIVERMODEL_API  int  DriverModelGetValue (long   type,
+                                           long   index1,
+                                           long   index2,
+                                           long   *long_value,
+                                           double *double_value,
+                                           char   **string_value)
+{
+  /* Gets the value of a data object of type <type>, selected by <index1> */
+  /* and possibly <index2>, and writes that value to <*double_value>,     */
+  /* <*float_value> or <**string_value> (object and value selection       */
+  /* depending on <type>).                                                */
+  /* Return value is 1 on success, otherwise 0.                           */
+
+  switch (type) {
+    case DRIVER_DATA_STATUS :
+      *long_value = 0;
+      return 1;
+    case DRIVER_DATA_VEH_TURNING_INDICATOR :
+      *long_value = turning_indicator;
+      return 1;
+    case DRIVER_DATA_VEH_DESIRED_VELOCITY   :
+      *double_value = desired_velocity;
+      return 1;
+    case DRIVER_DATA_VEH_COLOR :
+      *long_value = vehicle_color;
+      return 1;
+    case DRIVER_DATA_VEH_UDA :
+      return 0; /* doesn't set any UDA values */
+    case DRIVER_DATA_WANTS_SUGGESTION :
+      *long_value = 1;
+      return 1;
+    case DRIVER_DATA_DESIRED_ACCELERATION :
+      *double_value = desired_acceleration;
+      return 1;
+    case DRIVER_DATA_DESIRED_LANE_ANGLE :
+      *double_value = desired_lane_angle;
+      return 1;
+    case DRIVER_DATA_ACTIVE_LANE_CHANGE :
+      *long_value = active_lane_change;
+      return 1;
+    case DRIVER_DATA_REL_TARGET_LANE :
+      *long_value = rel_target_lane;
+      return 1;
+    case DRIVER_DATA_SIMPLE_LANECHANGE :
+      *long_value = 1;
+      return 1;
+    case DRIVER_DATA_USE_INTERNAL_MODEL:
+      *long_value = 1; /* must be set to 0 if external model is to be applied */
+      return 1;
+    case DRIVER_DATA_WANTS_ALL_NVEHS:
+      *long_value = 0; /* must be set to 1 if data for more than 2 nearby vehicles per lane and upstream/downstream is to be passed from Vissim */
+      return 1;
+    case DRIVER_DATA_ALLOW_MULTITHREADING:
+      *long_value = 0; /* must be set to 1 to allow a simulation run to be started with multiple cores used in the simulation parameters */
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+/*==========================================================================*/
+
+DRIVERMODEL_API  int  DriverModelExecuteCommand (long number)
+{
+  /* Executes the command <number> if that is available in the driver */
+  /* module. Return value is 1 on success, otherwise 0.               */
+
+  switch (number) {
+    case DRIVER_COMMAND_INIT :
+		fout.open("data_put_out_HV.txt", std::ios_base::out);
+		fout.flush();
+		for (int i = 0; i < veh_data.size(); i++)
+			veh_data[i].resize(0);
+      return 1;
+    case DRIVER_COMMAND_CREATE_DRIVER :
+      return 1;
+    case DRIVER_COMMAND_KILL_DRIVER :
+      return 1;
+    case DRIVER_COMMAND_MOVE_DRIVER :
+
+		veh_data[veh_id].push_back(current_speed);
+		fout.precision(4);
+		if (time_run == 300 && www == 0)
+		{
+			for (int i = 0; i < veh_data.size(); i++)
+			{
+				
+				//fout << "630   ";
+				if (veh_data[i].empty())
+				{
+					;
+				}
+				else
+				{
+					fout << veh_type << "   ";
+					for (int j = 0; j < veh_data[i].size(); j++)
+					{
+						fout << veh_data[i][j] << "  ";
+					}
+					fout << endl;
+				}
+			}
+
+			www = 1;
+		}
+      return 1;
+    default :
+      return 0;
+  }
+}
+
+/*==========================================================================*/
+/*  End of DriverModel.cpp                                                  */
+/*==========================================================================*/
